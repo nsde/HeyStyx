8ab0845a11227aa21538aacba71c709eeefad666
Felix <67185896+nsde@users.noreply.github.com>
2020-08-03 13:25:02 +0200

    +Imports +Kommentare +Versionseinstellungen

---
+++ main/main.py
@@ -0,0 +1,13 @@
+appVersion = 1.0
+appStatus = 'Alpha'
+
+# HeyStyx Discord Bot
+# For information, see: github.com/nsde/heystyx/
+# Created in 2020 with <3 by:
+# Felix Orosz (github@nsde) and Benedikt (github@beban09)
+
+import json
+import discord as dc
+import requests as rq
+import wikipedia as wiki
+from random import randint as rdi
